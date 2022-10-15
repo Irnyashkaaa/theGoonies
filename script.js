e6c51600be9d4f09191adba938a5c30a0b545ab1
Iryna <irynafurhala@gmail.com>
2022-10-15 16:33:34 +0300

    create

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+let green = document.getElementById('firstBlockGreen')
+let firstBlockBackground = document.getElementById('firstBlockBackground')
+let firstBlockText = document.getElementById('firstBlockText')
+let hoverTextBlock = document.getElementById('hoverTextBlock')
+let hoverTextBlockTitle = document.getElementById('hoverTextBlockTitle')
+let hoverTextBlockContent = document.getElementById('hoverTextBlockContent')
+let firstBlockLine = document.getElementById('firstBlockLine')
+
+
+let bigElement = document.getElementById('bigElement')
+let coin1 = document.getElementById('coin1')
+let coin2 = document.getElementById('coin2')
+let coin3 = document.getElementById('coin3')
+let coin4 = document.getElementById('coin4')
+
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY
+    green.style.width = 100 + scrollTop + '%'
+    green.style.left = -7 * scrollTop + 'px'
+    green.style.top = -3 * scrollTop + 'px'
+
+    firstBlockText.style.opacity = 1 - scrollTop / 300
+    firstBlockBackground.style.opacity = 1 - scrollTop / 500
+
+    hoverTextBlock.style.opacity = scrollTop / 200
+    if (scrollTop >= 1305) {
+
+        coin1.style.transform = 'scale(0.4) rotate(' + scrollTop / 200 % Math.PI + 'rad)';
+        coin1.style.top = scrollTop - 1400 + 'px'
+
+        coin2.style.transform = 'scale(0.5) rotate(' + -scrollTop / 200 % Math.PI + 'rad)'
+        coin2.style.top = 1000 - scrollTop / 2.2 + 'px'
+
+        coin3.style.transform = `scale(0.4) rotate(${scrollTop / 200 % Math.PI}rad)`
+    } else if (scrollTop >= 450) {
+        hoverTextBlock.style.opacity = 1 - scrollTop / 700
+    } else if (scrollTop >= 400) {
+        green.style.display = `none`
+        firstBlockLine.style.height = scrollTop / 10 * 3 + 'px'
+    } else {
+        green.style.display = `block`
+    }
+
+
+})
